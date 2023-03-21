5230861a0e4f32f92972a906880b250967bfd358
Amin Nateghi <amin.programmer@gmail.com>
2023-03-21 10:59:57 -0400

    added size prop to button

--- src/lib/button/button.tsx
+++ src/lib/button/button.tsx
@@ -1,11 +1,15 @@
+import { SizeType } from "../utils/global-types";
+import { joinClassName } from "../utils/global-function";
+import { PropsWithChildren } from "react";
+
 interface Props {
-  label: string;
+  size?: SizeType;
 }
 
-export const Button = ({ label }: Props) => {
-  return (
-    <div>
-      {label}
-    </div>
-  );
+export const Button = ({
+  size = "md",
+  children,
+}: Props & PropsWithChildren) => {
+  const classNames = joinClassName('button', `size-${size}`);
+  return <button className={classNames}>{children}</button>;
 };
